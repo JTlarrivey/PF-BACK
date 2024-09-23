1efa0c2ab540a3b6fdf05cabfcf1baaf5b41467f
JTlarrivey <joaqlarrivey@gmail.com>
2024-09-23 10:35:39 -0300

    cambioal main

--- main.ts
+++ main.ts
@@ -5,12 +5,19 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    // Configuración de CORS
+    app.enableCors({
+        origin: '*', // Permitir todas las solicitudes (puedes modificarlo para dominios específicos)
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true, // Si necesitas permitir cookies o autenticación
+    });
+
     const options = new DocumentBuilder()
-    .setTitle('Books API')
-    .setDescription('The books API description')
-    .setVersion('1.0.0')
-    .addBearerAuth()
-    .build();
+        .setTitle('Books API')
+        .setDescription('The books API description')
+        .setVersion('1.0.0')
+        .addBearerAuth()
+        .build();
 
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
