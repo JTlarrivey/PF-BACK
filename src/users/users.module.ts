81b38b076612abdda56408ef7c94c37160759520
Javier Del Rio <javier.ignaciodr1@gmail.com>
2024-10-04 18:39:07 -0300

    integracion cloudinary

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { FileUploadService } from 'src/file-upload/file-upload.service';
+import { FileUploadRepository } from 'src/file-upload/file.upload.repository';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, FileUploadService, FileUploadRepository],
 })
 export class UsersModule {}
